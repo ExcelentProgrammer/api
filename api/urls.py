4deca08ae6c6a5fe1ffdf0013d2c9e94dffeca36
Samandar <azamov.samandar.programmer@gmail.com>
2023-02-04 02:05:13 +0500

    update environs

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import tgBotView, test
 
 urlpatterns = [
-    path("sendMessageToken", tgBotView.as_view(), name="sendMessageToken"),
+    path("createToken/", tgBotView.as_view(), name="createToken"),
     path("test/", test, name="test"),
 ]
